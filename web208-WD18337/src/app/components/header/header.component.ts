0db811e4b5ecae926199990c83aa53731bf13b84
quanhn4002 <quannhph36246@fpt.edu.vn>
2024-05-17 16:32:59 +0700

    .

--- web208-WD18337/src/app/components/header/header.component.ts
+++ web208-WD18337/src/app/components/header/header.component.ts
@@ -4,54 +4,57 @@ import { IMenu } from '../../../interface/menu';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  menus:IMenu[] = [
+  menus: IMenu[] = [
     {
-      id:1,
-      name:'Home',
-      path:'home',
-      parent:0
+      id: 1,
+      name: 'Home',
+      path: 'home',
+      parent: 0,
     },
     {
-    id:2,
-    name:'About us',
-    path:'about-us',
-    parent:0
+      id: 2,
+      name: 'About us',
+      path: 'about-us',
+      parent: 0,
     },
     {
-    id:3,
-    name:'Products',
-    path:'product',
-    parent:0
+      id: 3,
+      name: 'Products',
+      path: 'product',
+      parent: 0,
     },
     {
-    id:4,
-    name:'Fashion',
-    path:'fashion',
-    parent:3
+      id: 4,
+      name: 'Fashion',
+      path: 'fashion',
+      parent: 3,
     },
     {
-    id:5,
-    name:'Jewelry',
-    path:'jewelry',
-    parent:3
+      id: 5,
+      name: 'Jewelry',
+      path: 'jewelry',
+      parent: 3,
     },
     {
-    id:6,
-    name:'Contact',
-    path:'contact',
-    parent:0
-    }
-  ]
-  checkChildrent = (menuid:number):boolean=>{
-    let check:boolean = false;
-    for (let item of this.menus){
-        if (item.parent===menuid){
-          check = true;
-        }
+      id: 6,
+      name: 'Contact',
+      path: 'contact',
+      parent: 0,
+    },
+  ];
+  checkChildrent = (menuid: number): boolean => {
+    //check menu có chứa menu con không
+    let check: boolean = false; //biến check
+    for (let item of this.menus) {
+      //duyệt qua các menu
+      if (item.parent === menuid) {
+        //nếu menu đó có parent = menuid thì trả về true
+        check = true;
+      }
     }
     return check;
-  }
+  };
 }
